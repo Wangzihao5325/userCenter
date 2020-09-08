71b22ad15682549a659118b6fdae26cb2bfff989
Wangzihao5325 <416990273@qq.com>
2020-09-09 01:53:50 +0800

    Updates

---
+++ precache-manifest.0b7583699459eb564b31f98dfd65a1c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665f825326a519f057d02840da1f88ee",
+    "url": "/userCenter/index.html"
+  },
+  {
+    "revision": "c7551b2c4de358a97a93",
+    "url": "/userCenter/static/css/main.abed17fb.chunk.css"
+  },
+  {
+    "revision": "4d72c3a64dc698fde0a9",
+    "url": "/userCenter/static/js/2.483abf64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/userCenter/static/js/2.483abf64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7551b2c4de358a97a93",
+    "url": "/userCenter/static/js/main.418a4323.chunk.js"
+  },
+  {
+    "revision": "1c2f3234923ca3cc56d6",
+    "url": "/userCenter/static/js/runtime-main.e93a1243.js"
+  },
+  {
+    "revision": "91bf249f7030eb6b77f95ae99b2a55c0",
+    "url": "/userCenter/static/media/banner.91bf249f.png"
+  }
+]);
